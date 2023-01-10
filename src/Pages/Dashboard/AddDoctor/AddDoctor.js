f56ed7b555c341a4016d8cb2e9745b2d7a766f4f
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-10 12:08:26 +0600

    added File Type image field

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,10 +1,23 @@
+import { async } from '@firebase/util';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const { data: specialties = [], isLoading } = useQuery({
+        queryKey: ["specialty"],
+        queryFn: async () => {
+            const res = await fetch("http://localhost:4000/appointmentSpecialty");
+            const data = await res.json();
+            return data
+        }
+    })
+    console.log(specialties);
+
     const handleAddDoctor = data => {
         console.log(data);
+
     }
     return (
         <div className=' w-11/12 '>
@@ -42,13 +55,30 @@ const AddDoctor = () => {
                     <div className="form-control w-full max-w-xs">
                         <label className="label"> <span className="label-text">Specialty</span></label>
 
-                        <select className="select select-ghost w-full max-w-xs">
-                            <option>Pick the </option>
-                            <option>Svelte</option>
-                            <option>Vue</option>
-                            <option>React</option>
+                        <select
+                            {...register("specialty"
+                            )}
+                            className="select input-bordered w-full max-w-xs">
+                            {
+                                specialties.map(specialty => <option
+
+                                    key={specialty._id}>{specialty.name}</option>)
+                            }
                         </select>
 
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Name</span></label>
+                        <input type="file"
+                            {...register("image",
+                                { required: "Image is Required" }
+                            )}
+                            className="file-input file-input-bordered file-input-success w-full max-w-xs" />
+                        {
+                            errors.img &&
+                            <small className='text-red-500 mt-1 font-semibold'>{errors.img.message}</small>
+                        }
                     </div>
 
                     <input className='btn btn-accent w-full mt-5' value="Add Doctor" type="submit" />
@@ -58,5 +88,10 @@ const AddDoctor = () => {
         </div>
     );
 };
-
+/**
+ *  Three place to store image
+ * 1. third party image hosting server
+ * 2. File system of your server  (Ex, AWS)
+ * 3. MongoDb or others database   (Ex; base 64 system)
+ */
 export default AddDoctor;
