a395a9b84254388795e19936c3197bda4b68c281
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2023-01-10 22:43:23 +0600

    Handle payment processing error and success

--- src/Pages/Dashboard/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Dashboard/Payment/Payment.js
@@ -1,5 +1,11 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
     const booking = useLoaderData()
@@ -9,6 +15,11 @@ const Payment = () => {
             <h3 className="text-3xl">Payment for {treatment}</h3>
             <div className='mt-4'>
                 <p className='text-xl'>please pay <strong>${price} </strong> for your appointment on {appointmentDate} at {slot}</p>
+            </div>
+            <div className=' w-96 mt-6'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
             </div>
         </div>
     );
